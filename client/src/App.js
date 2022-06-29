550713dc63947546b7120c9840157be6d837f376
Daniel Kim <danielkim2711@gmail.com>
2022-06-29 22:52:52 +1200

    Fix InMemoryCache problem

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,28 @@ import Header from './components/Header';
 import Clients from './components/Clients';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: 'http://127.0.0.1:8000/graphql',
-  cache: new InMemoryCache(),
+  cache,
 });
 
 function App() {
