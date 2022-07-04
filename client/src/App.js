eefac91b279a8e2776a3060514fe8d322bb97dea
Daniel Kim <danielkim2711@gmail.com>
2022-07-04 21:56:17 +1200

    Add Project page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Header from './components/Header';
 import Home from './pages/Home';
+import Project from './pages/Project';
 import NotFound from './pages/NotFound';
 
 const cache = new InMemoryCache({
@@ -38,6 +39,7 @@ function App() {
           <div className='container'>
             <Routes>
               <Route path='/' element={<Home />} />
+              <Route path='/projects/:id' element={<Project />} />
               <Route path='*' element={<NotFound />} />
             </Routes>
           </div>
